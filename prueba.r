f26623e0e137473e44b70744a01046da3bd5cc83
juanguerrablas <jguerrablas@gmail.com>
2017-06-02 12:33:30 -0500

    Update prueba.r
    agregar examen

--- prueba.r
+++ prueba.r
@@ -8,7 +8,18 @@
 
 # INTRODUCCION ------------------------------------------------------------
 
+# vectores; numerico, logico, caracter
+# vector de longitud 0
 
+#Crea un vector num?rico de 3 elementos
+c(1,2,3)
+
+# Crea un vector l?gico de 5 elementos
+c(T,F,T,T,F)
+
+# Crea un vector de 4 cadenas de caracteres
+ 
+c("anchoveta","jurel","perico","merlusa")	
 # PREPROCESAMIENTO --------------------------------------------------------
 
 
